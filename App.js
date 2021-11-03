c498bd73724bcd8467915b32e235b270be18e6dd
Your Name <yiwenc22@gmail.com>
2021-11-03 01:27:35 -1000

    Add simple footer menu

--- App.js
+++ App.js
@@ -1,26 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function App() {
+function ChatBotScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView>
-      <Text>Hi</Text>
-      </ScrollView>
-    <View style={{marginBottom: 30}}>
-      <Text>Question</Text>
-    </View>
-    </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 30,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function TrendingScreen() {
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+  );
+}
+
+function FeedBackScreen() {
+  return (
+      <View style={{ marginTop: 25 }}>
+        <Text style={{ alignItems: 'center' }}>Feedback</Text>
+        <TextInput placeholder="Email"/>
+        <TextInput placeholder="Problem"/>
+        <TextInput placeholder="Description"/>
+        <Button title="Submit" />
+        
+      </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="ChatBot" component={ChatBotScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="chat" color={color} size={size} />
+          ),
+        }}
+      />
+
+          <Tab.Screen name="Trending" component={TrendingScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="trending-up" color={color} size={size} />
+          ),
+        }}
+      />
+          <Tab.Screen name="Feedback" component={FeedBackScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information" color={color} size={size} />
+          ),
+        }}
+      />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
+}
