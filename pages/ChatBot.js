e050ce5e78bb6a2c25aacc02448d6093a7717ab9
Your Name <yiwenc22@gmail.com>
2021-11-05 03:47:50 -1000

    small change

--- pages/ChatBot.js
+++ pages/ChatBot.js
@@ -26,12 +26,16 @@ class ChatBot extends Component {
     }));
     switch(messages[0].text) {
  
-        case 'What is Covid?':
+        case 'What is COVID?':
           this.sendBotResponse("COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, some people experience post-COVID conditions. Post-COVID conditions are a wide range of new, returning, or ongoing health problems people can experience more than four weeks after first being infected with the virus that causes COVID-19. Older people and those who have certain underlying medical conditions are more likely to get severely ill from COVID-19. Vaccines against COVID-19 are safe and effective.");
           break;
         
         case 'How can I prevent it?':
             this.sendBotResponse("Handwashing is one of the best ways to protect yourself and your family from getting sick, Wear Mask, and stand 6 ft apart.");
+          break;
+
+        case 'Hi':
+            this.sendBotResponse("Aloha");
           break;
    
         default:
