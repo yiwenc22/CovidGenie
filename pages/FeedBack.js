ac7249decf1588bac0fa48fc7ec77561df25ee00
Your Name <yiwenc22@gmail.com>
2021-12-10 02:50:35 -1000

    Add form function
    now is clear after pressing submit and record the information in form and print it in the console. However is working on web and andorid fine but iphone a little bit different

--- pages/FeedBack.js
+++ pages/FeedBack.js
@@ -1,95 +1,92 @@
-import React, { useState } from "react";
+import React, { useEffect, useCallback } from 'react';
 import { useForm } from "react-hook-form";
 import { Text, View, TextInput, Button, Pressable, StyleSheet, Modal, } from 'react-native';
 
 export default function Feedback() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  const [modalVisible, setModalVisible] = useState(false);
+  const { register, handleSubmit, reset, setValue } = useForm();
+  const onSubmit = useCallback(formData => {
+    reset({ email: '' });
+    reset({ problem: '' });
+    reset({ description: '' });
+    console.log(formData);
+    alert('Forms Have Be Sent');
 
-  console.log(watch("example"));
+  }, []);
+  const onChangeField = useCallback(
+      name => text => {
+        setValue(name, text);
+      },
+      []
+  );
+
+  useEffect(() => {
+    register('email');
+    register('problem');
+    register('description');
+  }, [register]);
 
   return (
-      
-      <View style={{ marginTop: 100, alignItems: 'center' }}>
-              <Modal
-        animationType="slide"
-        transparent={false}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View>
-            <Text style={styles.modalText}>Feedback Sent</Text>
-            <Button title="Close" onPress={handleSubmit(onSubmit),() => setModalVisible(false)}/>
-          </View>
-        </View>
-      </Modal>
+      <View style={styles.centeredView}>
         <Text style={{ alignItems: 'center', fontSize: 45, marginTop: 125, fontWeight: '500' }}>Feedback</Text>
         <Text style={{ alignItems: 'center', fontSize: 16, marginTop: 20, fontWeight: '400' }}>Email</Text>
-        <TextInput style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height: 30}} 
-        //{...register("example")} 
-        placeholder="Email"
+        <TextInput
+            {...register("email", { required: false })}
+            style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height: 30}}
+            autoCompleteType="email"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            placeholder="Email"
+            onChangeText={onChangeField('email')}
         />
         <Text style={{ alignItems: 'center', fontSize: 16, marginTop: 20, fontWeight: '400' }}>Problem</Text>
-        <TextInput style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height: 30}}
-        //{...register("example")}  
-        placeholder="Problem"
+        <TextInput
+            {...register("problem", { required: false })}
+            style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height: 30}}
+            autoCompleteType="problem"
+            keyboardType="problem"
+            textContentType="problem"
+            placeholder="Problem"
+            onChangeText={onChangeField('problem')}
         />
         <Text style={{ alignItems: 'center', fontSize: 16, marginTop: 20, fontWeight: '400' }}>Description</Text>
-        <TextInput  multiline numberOfLines={4} style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height:100, marginBottom: 30 }} 
-        //{...register("example")} 
-        placeholder="Description"
+        <TextInput
+            {...register("description", { required: false })}
+            multiline
+            numberOfLines={4}
+            style={{ alignItems: 'center', borderWidth: 1, borderRadius: 5, width: 300, height:100, marginBottom: 30 }}
+            autoCompleteType="description"
+            keyboardType="description"
+            textContentType="description"
+            placeholder="Description"
+            onChangeText={onChangeField('description')}
         />
-        <Button title="Submit" onPress={handleSubmit(onSubmit),() => setModalVisible(true)}/>
-    </View>
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 20
-    },
-    modalView: {
-      margin: 25,
-      backgroundColor: "white",
-      borderRadius: 20,
-      padding: 500,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 20,
-        height: 20
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5
-    },
-    button: {
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2
-    },
-    buttonOpen: {
-      backgroundColor: "#F194FF",
-    },
-    buttonClose: {
-      backgroundColor: "#2196F3",
-    },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center"
-    },
-    modalText: {
-        marginTop: 25,
-      fontSize: 25
-    }
-  });
-  
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+});
+
